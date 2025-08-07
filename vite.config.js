6691fe899bd4ec4861138f8cf13fb0197ad0b444
xh2v <acc.fr65789@gmail.com>
2025-08-07 23:50:30 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: 'public',
   build: {
-    outDir: '../dist',
-    emptyOutDir: true
+    target: 'esnext',
+    lib: {
+      entry: 'src/index.ts',
+      formats: ['es'],
+      fileName: () => 'index.js'
+    },
+    outDir: 'dist',
+    emptyOutDir: true,
+    minify: true
   }
 })
